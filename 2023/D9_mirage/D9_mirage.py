c9cf14ab9d5771d7633d348231a290cc73c0f16c
Lamaison Cyril <cyril.lamaison@outlook.com>
2023-12-09 11:32:33 +0100

    D9 - P2

--- 2023/D9_mirage/D9_mirage.py
+++ 2023/D9_mirage/D9_mirage.py
@@ -9,8 +9,6 @@ logging.basicConfig(level = level, format=logging_format)
 def get_next_value(list):
     logging.debug(list)
     result = []
-    if list[len(list)-1] == 7111285:
-        test = 1
     for i in range(0, len(list)-1):
         result.append(int(list[i+1]) - int(list[i]))
     if all(ele == 0 for ele in result):
@@ -19,13 +17,26 @@ def get_next_value(list):
         tmp = get_next_value(result)
         return tmp + int(list[len(list)-1])
 
+def get_first_value(list):
+    logging.debug(list)
+    result = []
+    for i in range(0, len(list)-1):
+        result.append(int(list[i+1]) - int(list[i]))
+    if all(ele == 0 for ele in result):
+        return list[0]
+    else:
+        tmp = get_first_value(result)
+        return int(list[0]) - tmp
 
 file_path = '2023\\D9_mirage\\test1.txt'
 file_path = '2023\\D9_mirage\\test_final.txt'
 result_p1 = 0
+result_p2 = 0
 with open(file=file_path, mode='r') as f:
     file_text = f.read().split('\n')
-    for row in file_text:
-        result_p1 += get_next_value(list=row.split())
+for row in file_text:
+    result_p1 += get_next_value(list=row.split())
 logging.info(f'-----------------result P1: {result_p1}')
-# logging.info(f'-----------------result P2: {result_p2}')
+for row in file_text:
+    result_p2 += get_first_value(list=row.split())
+logging.info(f'-----------------result P2: {result_p2}')
